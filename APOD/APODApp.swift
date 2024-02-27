d56c92d5e5d59dff86abe9d486e4d303ee66e5ed
rodrigoorozco <rodrigoorozco025@gmail.com>
2024-02-26 21:13:12 -0600

    api key added/gitignore mod

---
+++ APOD/APODApp.swift
@@ -0,0 +1,17 @@
+//
+//  APODApp.swift
+//  APOD
+//
+//  Created by rodrigo Orozco  on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct APODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
