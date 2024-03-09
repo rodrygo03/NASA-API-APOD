10d081ed0b8ccf5e7803d12417d20e3605b40f99
RodrigoOrozco <rodrigoorozco025@gmail.com>
2024-03-09 12:57:44 -0600

    Simple interface only supports images

--- APOD/ContentView.swift
+++ APOD/ContentView.swift
@@ -7,18 +7,59 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var picOfDay = APOD()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 3) {
+            Text(picOfDay.date)
+                .font(.headline)
+            Text(picOfDay.title)
+                .font(.title)
+                .multilineTextAlignment(.center)
+            AsyncImage(url: URL(string: picOfDay.hdurl)) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                }
+                else if phase.error != nil  {
+                    Text("Error loading image")
+                }
+                else {
+                    ProgressView()
+                }
+            }
         }
-        .padding()
+
+
+        ScrollView  {
+            VStack {
+                Text(picOfDay.explanation)
+                    .lineSpacing(8)
+                    .padding([.horizontal, .bottom])
+            }
+        }
+
+        .task {
+            do {
+                try await picOfDay = fetchAPOD()
+                print("Success")
+            } catch CallError.fileNotFound {
+                print("file not found")
+            } catch CallError.contentError {
+                print("error in file contents")
+            } catch CallError.invalidResponse {
+                print("Non 200 status code")
+            } catch CallError.invalidURL {
+                print("Error with URL")
+            } catch {
+                print("Other error")
+            }
+        }
+
     }
 }
-
 #Preview {
     ContentView()
 }
